c0d09999fff879b6430c84990879af2572d5d1df
James Scanlan <djamesscanlan@gmail.com>
2017-11-29 07:32:47 +0000

    Tidy up and modification to make load_grands_prix_meta_data.py OS agnostic

--- f1ftw/load_grands_prix_meta_data.py
+++ f1ftw/load_grands_prix_meta_data.py
@@ -5,7 +5,7 @@ import datetime
 
 def ReadGrandsPrixMetaData():
     grands_prix_meta_data = objects.grands_prix_meta_data.GrandsPrixMetaData()
-    jsonData = json.load(open(os.path.join(os.path.abspath(".."), "/data", "GrandsPrixMetaData.json")))
+    jsonData = json.load(open(os.path.join(os.path.abspath(".."), "data", "GrandsPrixMetaData.json")))
     for grand_prix in jsonData["GrandsPrix"]:
         grands_prix_meta_data.AddObject(objects.grands_prix_meta_data.GrandPrixMetaData(grand_prix["Name"], datetime.datetime.strptime(grand_prix["StartDate"],"%Y-%m-%d").date(), datetime.datetime.strptime(grand_prix["EndDate"],"%Y-%m-%d").date()))
     return grands_prix_meta_data
